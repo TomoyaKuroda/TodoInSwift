7a642af8ac3e4ca9b6660bf24d58a629342acf0f
Tomoya <200378857@student.georgianc.on.ca>
2019-07-26 17:30:48 -0400

    Initial Commit

---
+++ TodoWithFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoWithFirebase
+//
+//  Created by user user on 2019/07/26.
+//  Copyright © 2019 Group8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
