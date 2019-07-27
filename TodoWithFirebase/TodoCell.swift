4819eba6ba35ec963bc376415fbc2de27b6a30e2
Tomoya <200378857@student.georgianc.on.ca>
2019-07-27 00:24:49 -0400

    first commit

---
+++ TodoWithFirebase/TodoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoCell.swift
+//  TodoWithFirebase
+//
+//  Created by user user on 2019/07/26.
+//  Copyright © 2019 Group8. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TodoWithFirebase/TodoView.swift
@@ -0,0 +1,30 @@
+//
+//  TodoView.swift
+//  TodoWithFirebase
+//
+//  Created by user user on 2019/07/26.
+//  Copyright © 2019 Group8. All rights reserved.
+//
+
+import UIKit
+
+class TodoView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
