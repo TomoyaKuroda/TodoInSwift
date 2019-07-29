b8bd9c80b8bcc303df54e744a7b881e99b0b367c
Dongjun Yu <200372591@ba-lc5-2401.student.georgianc.on.ca>
2019-07-29 18:23:57 -0400

    Re-designed login page

---
+++ TodoWithFirebase/ThirdViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ThirdViewController.swift
+//  TodoWithFirebase
+//
+//  Created by Dongjun Yu on 2019-07-29.
+//  Copyright © 2019 Group8. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class ThirdViewController: UIViewController {
+
+    @IBOutlet weak var textEmail: UITextField!
+    
+    var uid: String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textEmail.text = ""
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textEmail.text = ""
+    }
+    
+    
+    @IBAction func submitAction(_ sender: Any) {
+        if self.textEmail.text == "" {
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter an email.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().sendPasswordReset(withEmail: self.textEmail.text!, completion: { (error) in
+                
+                var title = ""
+                var message = ""
+                
+                if error != nil {
+                    title = "Error!"
+                    message = (error?.localizedDescription)!
+                } else {
+                    title = "Success!"
+                    message = "Password reset email sent."
+                    self.textEmail.text = ""
+                }
+                
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            })
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigation = segue.destination as! UINavigationController
+        let todoVC = navigation.topViewController as! TodoView
+        todoVC.userID = uid
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TodoWithFirebase/ViewController.swift
+++ TodoWithFirebase/ViewController.swift
@@ -29,31 +29,28 @@ class ViewController: UIViewController {
         textPassword.text = ""
     }
 
-    @IBAction func registerAction(_ sender: Any) {
-        if textEmail.text != nil && textPassword.text != nil {
-            Auth.auth().createUser(withEmail: textEmail.text!, password: textPassword.text!) { (result, error) in
-                if error != nil {
-                    print("THERE WAS AN ERROR")
-                }
-                else{
-                    self.uid = (result?.user.uid)!
-                    let ref = Database.database().reference(withPath: "users").child(self.uid)
-                    ref.setValue(["email": self.textEmail.text!, "password": self.textPassword.text!])
-                    self.performSegue(withIdentifier: "loginSegue", sender: self)
-                }
-            }
-        }
-    }
+    
     
     @IBAction func signinAction(_ sender: Any) {
-        if textEmail.text != nil && textPassword.text != nil {
-            Auth.auth().signIn(withEmail: textEmail.text!, password: textPassword.text!) { (result, error) in
-                if error != nil {
-                    print("THERE WAS AN ERROR")
-                }
-                else{
+        if self.textEmail.text == "" || self.textPassword.text == "" {
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().signIn(withEmail: self.textEmail.text!, password: self.textPassword.text!) { (result, error) in
+                if error == nil {
                     self.uid = (result?.user.uid)!
                     self.performSegue(withIdentifier: "loginSegue", sender: self)
+                } else {
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
                 }
             }
         }
